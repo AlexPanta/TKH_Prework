f04a879d35fa2fcc810845f5f420155ef850a556
AlexPanta <86111796+AlexPanta@users.noreply.github.com>
2021-06-18 18:55:09 -0400

    Create assignment_2.py

---
+++ assignment_2.py
@@ -0,0 +1,17 @@
+example = 25
+if example > 26:
+	print “over”
+else
+	print “under”
+nums = [1, 45, 32, 21, 5, 17, 43, 93]
+for nums in num:
+print num
+#Output:
+#1
+#45
+#32
+#21
+#5
+#17
+#43
+#93
