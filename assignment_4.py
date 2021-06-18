93c0ee3fabef1cdcf20e600f082fb2dc345635e0
AlexPanta <86111796+AlexPanta@users.noreply.github.com>
2021-06-18 19:04:00 -0400

    Create assignment_4.py

---
+++ assignment_4.py
@@ -0,0 +1,9 @@
+names_list = [“bob”, “jimmy”, “max b”, “bernie”, “jordan”, “future hendrix”]
+largest_name = ‘’
+if len(name) > largest_name:
+	print “largest_name”
+def largest name (name1, name2, name3, name4, name5, name6)
+	return name6
+  print “largest_name”
+#save “variable big_name”
+  print “variable big_name” 
