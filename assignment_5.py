19c0215d03307386638117731122308da66c61e0
AlexPanta <86111796+AlexPanta@users.noreply.github.com>
2021-06-18 19:05:05 -0400

    Create assignment_5.py

---
+++ assignment_5.py
@@ -0,0 +1,11 @@
+assignment_5.py 
+names_list = [“bob”,”jimmy”,”max b”, “bernie”, “jordan”,”future hendrix”]
+names_even = [”max b”]
+names_odd = [“bob”,”jimmy”,“bernie”,“jordan”,”future hendrix”]
+revised_even = [“bax b”]
+revised_odd = [“boc”,”jimmc”,“bernic”,“jordac”,”future hendric”]
+	print “revised_even”
+	print “revised_odd”
+def even_list (names_even,revised_even):
+	return even_list
+print “even_list”
